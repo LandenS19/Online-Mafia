6d8f01e0ab55ba9e7dc7cbc053d986b22734f69e
LandenS19 <landen.summers19@gmail.com>
2026-03-06 15:25:22 -0600

    updates for testing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,13 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from typing import Annotated
+
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool | None = None
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_price": item.price, "item_id": item_id}
+@app.get("/items/")
+async def read_items(
+    q: Annotated[str | None, Query(min_length=3)] = "fixedquery"):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
